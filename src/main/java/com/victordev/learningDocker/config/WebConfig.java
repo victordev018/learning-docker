7a3bff4fdfced38cc68468f7f8b9c6197e3e4aa7
João VIctor <joawrt8520@gmail.com>
2024-12-02 21:48:32 -0300

    feat: add cors configuration

---
+++ src/main/java/com/victordev/learningDocker/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.victordev.learningDocker.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**");     // any mapping will access to the routes
+    }
+}
